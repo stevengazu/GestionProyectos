dfb99cf7b7c61adaf056f99e9e7ba8e4c15c3f98
stevengazu <59656403+stevengazu@users.noreply.github.com>
2024-06-28 08:41:15 -0600

    Update index.ts
    Add Class A

--- index.ts
+++ index.ts
@@ -20,3 +20,16 @@ export const displayWeather = async () => {
 if (buttonClick) buttonClick.addEventListener('click', displayWeather);
 
 
+//class A
+class ClaseA {
+    foo: number;
+    bar: string;
+
+    constructor(foo: number, bar: string) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+}
+const instanciaA = new ClaseA(42, "Hola, mundo");
+console.log(instanciaA.foo); // 42
+console.log(instanciaA.bar); // "Hola, mundo"
